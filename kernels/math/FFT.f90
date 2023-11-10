5df0145344c5caf840c0cb3fce88e629ce637973
kvorka <jkdarkorbit@gmail.com>
2023-11-10 16:11:48 +0100

    heavy cleaning in FFTinit, previous commit convection benchmarked

--- kernels/math/FFT.f90
+++ kernels/math/FFT.f90
@@ -2,7 +2,7 @@ module FFT_mod
   !Author of the original code: Keiichi Ishioka
   !Original work: fxpack (ISPACK FORTRAN SUBROUTINE LIBRARY FOR SCIENTIFIC COMPUTING)
   use Math
-  implicit none ; private
+  implicit none
   
   type, public :: T_fft
     integer                     :: n
@@ -21,13 +21,16 @@ module FFT_mod
     
   end type T_fft
   
-  integer,        parameter :: imm = -2e4
-  real(kind=dbl), parameter :: C31 = -0.5_dbl
-  real(kind=dbl), parameter :: C32 = +0.86602540378443864676_dbl
-  real(kind=dbl), parameter :: C51 = +0.25_dbl
-  real(kind=dbl), parameter :: C52 = +0.5590169943749474241_dbl
-  real(kind=dbl), parameter :: C53 = +0.6180339887498948482_dbl
-  real(kind=dbl), parameter :: C54 = -0.9510565162951535721_dbl
+  integer,        parameter, private :: imm = -2e4
+  real(kind=dbl), parameter, private :: C31 = -0.5_dbl
+  real(kind=dbl), parameter, private :: C32 = +0.86602540378443864676_dbl
+  real(kind=dbl), parameter, private :: C51 = +0.25_dbl
+  real(kind=dbl), parameter, private :: C52 = +0.5590169943749474241_dbl
+  real(kind=dbl), parameter, private :: C53 = +0.6180339887498948482_dbl
+  real(kind=dbl), parameter, private :: C54 = -0.9510565162951535721_dbl
+  
+  private :: fft_init_sub, fft_deallocate_sub, fft_r2c_exec_sub, fft_c2r_exec_sub, fft_r2c_sub, fft_c2r_sub, fxzini, fxztal, &
+           & fxzm2a, fxzm2b, fxzm3a, fxzm3b, fxzm4a, fxzm4b, fxzm5a, fxzm5b
   
   contains
   
@@ -37,10 +40,10 @@ module FFT_mod
     integer                     :: i
     
     this%n = n
-    allocate( this%it(n/2)  ) ; this%it = 0
-    allocate( this%t(3*n/2) ) ; this%t = 0._dbl
+      allocate( this%it(n/2)  ) ; this%it = 0
+      allocate( this%t(3*n/2) ) ; this%t = 0._dbl
     
-    call FXZINI(n/2, this%it, this%t)
+    call fxzini(n/2, this%it, this%t)
     
     do i = 1, (n-2) / 4
       this%t(n+2*i-1) = cos(2 * pi * i / n)
@@ -98,16 +101,16 @@ module FFT_mod
     
     select case ( mod(this%it(this%n/2-1),4)+2 )
       case (4)
-        call FXZM4B(m, this%n/2, x)
+        call fxzm4b(m, this%n/2, x)
       case (2)
-        call FXZM2B(m, this%n/2, x)
+        call fxzm2b(m, this%n/2, x)
       case (3)
-        call FXZM3B(m, this%n/2, x)
+        call fxzm3b(m, this%n/2, x)
       case (5)
-        call FXZM5B(m, this%n/2, x)
+        call fxzm5b(m, this%n/2, x)
     end select
     
-    call FXZTAL(m, 1, this%n/2, x, this%t, 1, this%it(this%n/2), 0, 0, this%it(this%n/2-1))
+    call fxztal(m, 1, this%n/2, x, this%t, 1, this%it(this%n/2), 0, 0, this%it(this%n/2-1))
     
     allocate( y(m,2) ) ; j = 1
       
@@ -211,16 +214,16 @@ module FFT_mod
     
     select case ( mod(this%it(this%n/2-1),4)+2 )
       case (4)
-        call FXZM4B(m, this%n/2, x)
+        call fxzm4b(m, this%n/2, x)
       case (2)
-        call FXZM2B(m, this%n/2, x)
+        call fxzm2b(m, this%n/2, x)
       case (3)
-        call FXZM3B(m, this%n/2, x)
+        call fxzm3b(m, this%n/2, x)
       case (5)
-        call FXZM5B(m, this%n/2, x)
+        call fxzm5b(m, this%n/2, x)
     end select
     
-    call FXZTAL(m, 1, this%n/2, x, this%t, 1, this%it(this%n/2), 0, 0, this%it(this%n/2-1))
+    call fxztal(m, 1, this%n/2, x, this%t, 1, this%it(this%n/2), 0, 0, this%it(this%n/2-1))
     
     allocate( y(m,2) ) ; j = 1
       
@@ -267,142 +270,115 @@ module FFT_mod
     
   end subroutine fft_deallocate_sub
   
-  subroutine FXZINI(n, it, t)
-   integer :: N,IT(N),J,K,L,I,IS,IC,I4,ITSUM,IT1,ISJ
-   integer :: IPN(4)=(/5,4,3,2/),IPC,IP,IPD
-   integer :: ITC(2:5)
-   REAL(kind=dbl) :: T(2,0:N-1)    
-   integer,DIMENSION(:),ALLOCATABLE :: ITW,IW
- 
-   ALLOCATE(ITW(0:N-1))
- 
-   ITC=0
-   
-   J=N
-   DO I=5,2,-1
-      ITC(I)=0
-      K=0
-      DO WHILE(K.EQ.0)
-         K=MOD(J,I)
-         IF(K.EQ.0) THEN
-            ITC(I)=ITC(I)+1
-            J=J/I
-         END IF
-      END DO
-   END DO
-   IF(J.NE.1) THEN
-      PRINT*, '*** error in FXZINI: N.ne.(2**P)*(3**Q)*(5**R)'
-      STOP
-   END IF
- 
-   ITW=0
-   K=N
-   L=1
-   DO IPC=4,1,-1
-      IP=IPN(IPC)
-      DO IC=1,ITC(IP)
-         DO I=0,L-1      
-            DO J=0,K/IP-1
-               DO IPD=1,IP-1
-                  ITW(I+L*(IPD+J*IP))=ITW(I+L*(IPD+J*IP))+IPD*K/IP
-               END DO
-            END DO
-         END DO
-         K=K/IP
-         L=L*IP
-      END DO
-   END DO
- 
-   T=0
-   K=N
-   L=1
-   IS=0
-   DO IPC=1,4
-      IP=IPN(IPC)
-      DO IC=1,ITC(IP)
-         DO I=0,L-1
-            J=ITW(K*I)
-            DO IPD=1,IP-1
-               T(1,IS+(IP-1)*I+IPD-1)=COS(2*PI*J*IPD/(IP*L))
-               T(2,IS+(IP-1)*I+IPD-1)=SIN(2*PI*J*IPD/(IP*L))
-            END DO
-         END DO
-         IS=IS+L*(IP-1)        
-         K=K/IP
-         L=L*IP
-      END DO
-   END DO
- 
-   ITW=0
-   K=N
-   L=1
-   DO IPC=1,4
-      IP=IPN(IPC)
-      DO IC=1,ITC(IP)
-         DO I=0,L-1      
-            DO J=0,K/IP-1
-               DO IPD=1,IP-1
-                  ITW(I+L*(IPD+J*IP))=ITW(I+L*(IPD+J*IP))+IPD*K/IP
-               END DO
-            END DO
-         END DO
-         K=K/IP
-         L=L*IP
-      END DO
-   END DO
- 
-   ITSUM=0
-   DO I=2,5
-      ITSUM=ITSUM+ITC(I)
-   END DO
- 
-   I4=1
-   IT1=0
-   DO IPC=1,4
-      IP=IPN(IPC)
-      DO IC=1,ITC(IP)
-         IT1=IT1+(IP-2)*I4
-         I4=I4*4
-      END DO
-   END DO
- 
-   IT(N-1)=IT1
-   IT(N)=ITSUM
- 
-   IF(N.GE.3) THEN
-      ALLOCATE(IW(N-2))
-      IW=0
-      IC=0
-      DO J=1,N-2
-         IF(IW(J).EQ.1) THEN
-            CYCLE
-         END IF
-         IC=IC+1
-         IT(IC)=J
-         IW(J)=1
-         ISJ=ITW(J)
-         DO
-            IF(ISJ.EQ.J) THEN
-               IT(IC)=IT(IC)+IMM
-               EXIT
-            END IF
-            IC=IC+1
-            IT(IC)=ISJ
-            IW(ISJ)=1             
-            ISJ=ITW(ISJ)
-         END DO
-      END DO
-      DEALLOCATE(IW)
-   END IF
+  subroutine fxzini(n, it, t)
+    integer,        intent(in)  :: n
+    integer,        intent(out) :: it(n)
+    real(kind=dbl), intent(out) :: t(2,0:n-1)
+    integer                     :: j, k, l, i, is, ic, i4, it1, isj, ipc, ip, ipd
+    integer,        allocatable :: ipn(:), itc(:), itw(:), iw(:)
+    
+    allocate( ipn(4), itc(2:5), itw(0:n-1) , iw(n-2) ) ; ipn = [5,4,3,2] ; t = 0._dbl ; iw = 0 ; itw = 0
+    
+    itc = 0 ; j = n
+      do i = 5, 2, -1
+        itc(i) = 0 ; k = 0
+        
+        do while ( k == 0 )
+          k = mod( j, i )
+          
+          if (k == 0) then
+            itc(i) = itc(i) + 1
+            j = j / i
+          end if
+        end do
+      end do
+    
+    if ( j /= 1 ) then
+      write(*,*) "*** error in fft init: n /= (2**P)*(3**Q)*(5**R)" ; stop
+    end if
+    
+    itw = 0 ; k = n ; l = 1
+      do ipc = 4, 1, -1
+        ip = ipn(ipc)
+        
+        do ic = 1, itc(ip)
+          do concurrent ( i = 0:l-1 , j = 0:k/ip-1 , ipd = 1:ip-1 )
+            itw(i+l*(ipd+j*ip)) = itw(i+l*(ipd+j*ip)) + ipd * k/ip
+          end do
+          
+          k = k / ip
+          l = l * ip
+        end do
+      end do
+    
+    k = n ; l = 1 ; is = 0
+      do ipc = 1, 4
+        ip = ipn(ipc)
+        
+        do ic = 1, itc(ip)
+          do i = 0, l-1
+            j = itw(k*i)
+            
+            do ipd = 1, ip-1
+              t(1,is+(ip-1)*i+ipd-1) = cos( 2 * pi * j * ipd / ( ip * l ) )
+              t(2,is+(ip-1)*i+ipd-1) = sin( 2 * pi * j * ipd / ( ip * l ) )
+            end do
+          end do
+          
+          is = is + l * (ip-1)        
+          k  = k / ip
+          l  = l * ip
+        end do
+      end do
+    
+    itw = 0 ; k = n ; l = 1
+      do ipc = 1, 4
+        ip = ipn(ipc)
+        
+        do ic = 1, itc(ip)
+          do concurrent ( i = 0:l-1 , j = 0:k/ip-1 , ipd = 1:ip-1 )
+            itw(i+l*(ipd+j*ip)) = itw(i+l*(ipd+j*ip)) + ipd * k/ip
+          end do
+          
+          k = k / ip
+          l = l * ip
+        end do
+      end do
+    
+    it1 = 0 ; i4 = 1
+      do ipc = 1, 4
+        ip = ipn(ipc)
+        
+        do ic = 1, itc(ip)
+          it1 = it1 + (ip-2) * i4 ; i4 = i4 * 4
+        end do
+      end do
+    
+    it(n-1) = it1 ; it(n) = sum( itc ) ; ic = 0
+      do j = 1, n-2
+        if ( iw(j) /= 1 ) then
+          ic = ic + 1 ; it(ic) = j ; iw(j) = 1 ; isj = itw(j)
+            do
+              if ( isj == j ) then
+                it(ic) = it(ic) + imm ; exit
+              end if
+              
+              ic      = ic + 1
+              it(ic)  = isj
+              iw(isj) = 1             
+              isj     = itw(isj)
+            end do
+        end if
+      end do
       
-   DEALLOCATE(ITW)
- 
-  end subroutine FXZINI
+    deallocate( itw , iw )
+    
+  end subroutine fxzini
   
-  pure recursive subroutine FXZTAL(m, k, l, x, t, ic, itsum, is, j1, it1)
+  pure recursive subroutine fxztal(m, k, l, x, t, ic, itsum, is, j1, it1)
     integer,        intent(in)    :: m, k, l, ic, itsum, is, j1, it1
-    real(kind=dbl), intent(in)    :: T(2,0:*)
-    real(kind=dbl), intent(inout) :: X(M,2,0:*)
+    real(kind=dbl), intent(in)    :: t(2,0:*)
+    real(kind=dbl), intent(inout) :: x(m,2,0:*)
     integer                       :: ip, j, isd, kd, ld, k1, jd, icd, icdd, it1d
     
     kd = k ; ld = l ; k1 = 1 ; j = j1 ; it1d = it1 ; ip = mod(it1d,4)+2
@@ -410,13 +386,13 @@ module FFT_mod
     if ( ic /= 1 ) then
       select case (ip)
         case (4)
-          call FXZM4A(m, k1, ld, x(1,1,j*ld), t(1,is+j*(ip-1)))
+          call fxzm4a(m, k1, ld, x(1,1,j*ld), t(1,is+j*(ip-1)))
         case (2)
-          call FXZM2A(m, k1, ld, x(1,1,j*ld), t(1,is+j*(ip-1)))
+          call fxzm2a(m, k1, ld, x(1,1,j*ld), t(1,is+j*(ip-1)))
         case (3)
-          call FXZM3A(m, k1, ld, x(1,1,j*ld), t(1,is+j*(ip-1)))
+          call fxzm3a(m, k1, ld, x(1,1,j*ld), t(1,is+j*(ip-1)))
         case (5)
-          call FXZM5A(m, k1, ld, x(1,1,j*ld), t(1,is+j*(ip-1)))
+          call fxzm5a(m, k1, ld, x(1,1,j*ld), t(1,is+j*(ip-1)))
       end select
     end if
     
@@ -424,7 +400,7 @@ module FFT_mod
     
     if ( (m * ip * ld > 2048) .and. (icd <= itsum) ) then
       do jd = 0, ip-1
-         call FXZTAL(m, kd, ld, x, t, icd, itsum, isd, j+jd, it1d)
+         call fxztal(m, kd, ld, x, t, icd, itsum, isd, j+jd, it1d)
       end do
     else
       do icdd = icd, itsum
@@ -432,22 +408,22 @@ module FFT_mod
         
         select case (ip)
           case (4)
-            call FXZM4A(m, k1, ld, x(1,1,j*ld), t(1,isd+j*(ip-1)))
+            call fxzm4a(m, k1, ld, x(1,1,j*ld), t(1,isd+j*(ip-1)))
           case (2)
-            call FXZM2A(m, k1, ld, x(1,1,j*ld), t(1,isd+j*(ip-1)))
+            call fxzm2a(m, k1, ld, x(1,1,j*ld), t(1,isd+j*(ip-1)))
           case (3)
-            call FXZM3A(m, k1, ld, x(1,1,j*ld), t(1,isd+j*(ip-1)))
+            call fxzm3a(m, k1, ld, x(1,1,j*ld), t(1,isd+j*(ip-1)))
           case (5)
-            call FXZM5A(m, k1, ld, x(1,1,j*ld), t(1,isd+j*(ip-1)))
+            call fxzm5a(m, k1, ld, x(1,1,j*ld), t(1,isd+j*(ip-1)))
         end select
         
         k1 = k1 * ip ; ld = ld/ip ; isd = isd + kd * (ip-1) ; kd = kd * ip ; j = j * ip ; it1d = it1d / 4
       end do
     end if
     
-  end subroutine FXZTAL
+  end subroutine fxztal
   
-  pure subroutine FXZM2A(m, k, l, x, t)
+  pure subroutine fxzm2a(m, k, l, x, t)
     integer,     intent(in)    :: m, k, l
     real(kind=dbl), intent(in)    :: t(2,0:*)
     real(kind=dbl), intent(inout) :: x(m,2,l/2,0:1,0:k-1)
@@ -463,9 +439,9 @@ module FFT_mod
        end do
     end do
   
-  end subroutine FXZM2A
+  end subroutine fxzm2a
   
-  pure subroutine FXZM2B(m, l, x)
+  pure subroutine fxzm2b(m, l, x)
     integer,        intent(in)    :: m, l
     real(kind=dbl), intent(inout) :: x(m,2,l/2,0:1)
     integer                       :: i, iv
@@ -475,9 +451,9 @@ module FFT_mod
       x(iv,1,i,0) = 2 * x(iv,1,i,0) - x(iv,1,i,1) ; x(iv,2,i,0) = 2 * x(iv,2,i,0) - x(iv,2,i,1)
     end do
     
-  end subroutine FXZM2B
+  end subroutine fxzm2b
   
-  pure subroutine FXZM3A(m, k, l, x, t)
+  pure subroutine fxzm3a(m, k, l, x, t)
     integer,        intent(in)    :: m, k, l
     real(kind=dbl), intent(in)    :: t(2,0:*)
     real(kind=dbl), intent(inout) :: x(m,2,l/3,0:2,0:k-1)
@@ -506,9 +482,9 @@ module FFT_mod
        ij = ij + 2
     end do
     
-  end subroutine FXZM3A
+  end subroutine fxzm3a
   
-  pure subroutine FXZM3B(m, l, x)
+  pure subroutine fxzm3b(m, l, x)
     integer,        intent(in)    :: m, l
     real(kind=dbl), intent(inout) :: x(m,2,l/3,0:2)
     integer                       :: i, iv
@@ -524,9 +500,9 @@ module FFT_mod
       x(iv,1,i,1) = 2 * x2re -       x(iv,1,i,2) ; x(iv,2,i,1) = 2 * x2im -       x(iv,2,i,2)
     end do
     
-  end subroutine FXZM3B
+  end subroutine fxzm3b
   
-  pure subroutine FXZM4A(m, k, l, x, t)
+  pure subroutine fxzm4a(m, k, l, x, t)
     integer,        intent(in)    :: m, k, l
     REAL(kind=dbl), intent(in)    :: t(2,0:*)
     real(kind=dbl), intent(inout) :: x(m,2,l/4,0:3,0:k-1)
@@ -558,9 +534,9 @@ module FFT_mod
       ij = ij + 3
     end do
     
-  end subroutine FXZM4A
+  end subroutine fxzm4a
   
-  pure subroutine FXZM4B(m, l, x)
+  pure subroutine fxzm4b(m, l, x)
     integer,        intent(in)    :: m, l
     real(kind=dbl), intent(inout) :: x(m,2,l/4,0:3)
     integer                       :: i, iv
@@ -578,9 +554,9 @@ module FFT_mod
       x(iv,1,i,3) = 2 * x2re - x(iv,1,i,1) ; x(iv,2,i,3) = 2 * x2im - x(iv,2,i,1)       
     end do
   
-  end subroutine FXZM4B
+  end subroutine fxzm4b
   
-  pure subroutine FXZM5A(m, k, l, x, t)
+  pure subroutine fxzm5a(m, k, l, x, t)
     integer,        intent(in)    :: m, k, l
     real(kind=dbl), intent(in)    :: t(2,0:*)
     real(kind=dbl), intent(inout) :: x(m,2,l/5,0:4,0:k-1)
@@ -624,9 +600,9 @@ module FFT_mod
        ij = ij + 4
     end do
   
-  end subroutine FXZM5A
+  end subroutine fxzm5a
   
-  pure subroutine FXZM5B(m, l, x)
+  pure subroutine fxzm5b(m, l, x)
     integer,        intent(in)    :: m, l
     real(kind=dbl), intent(inout) :: x(m,2,l/5,0:4)
     integer                       :: i, iv
@@ -654,6 +630,6 @@ module FFT_mod
       x(iv,1,i,1) = 2 * x4re        -       x(iv,1,i,4) ; x(iv,2,i,1) = 2 * x4im        -       x(iv,2,i,4)
     end do
     
-  end subroutine FXZM5B
+  end subroutine fxzm5b
   
 end module FFT_mod
