43d564ddb3210b3d2c208bbc34110355feb5c52a
kvorka <jkdarkorbit@gmail.com>
2023-11-15 15:29:20 +0100

    Harmsy error fix 2

--- kernels/math/Harmsy.f90
+++ kernels/math/Harmsy.f90
@@ -13,42 +13,38 @@ module Harmsy
     integer,            intent(in)  :: jmax_in
     complex(kind=dbl),  intent(in)  :: spectra_in(:)
     real(kind=dbl),     intent(out) :: data_out(:,:)
-    integer                         :: it, ip, j, m
+    integer                         :: it, ip, ij, im
     real(kind=dbl)                  :: costheta
     real(kind=dbl),    allocatable  :: polyLege(:)
     complex(kind=dbl), allocatable  :: sumLege(:), expphi(:), expmul(:)
     
-    data_out = 0._dbl
-    
     allocate( sumLege(0:jmax_in), expphi(2*nth), expmul(2*nth), polyLege(jmax_in+1) )
     
-    do it = 1, nth
-      costheta = cos(pi/nth * (it-0.5_dbl)) ; sumLege(:) = czero
-      
-      do m = 0, jmax_in
-        call dpmm_sub( costheta, jmax_in, m, polyLege )
-        
-        do j = m, jmax_in
-          sumLege(m) = sumLege(m) + spectra_in(j*(j+1)/2+m+1) * polyLege(j-m+1)
-        end do
-      end do
-      
-      do ip = 1, 2*nth
-        expphi(ip) = exp( cunit * (ip-1) * pi / nth )
-        expmul(ip) = cone
-      end do
-      
-      m = 0
-        data_out(:,it) = data_out(:,it) + c2r_fn( sumLege(m) )
-      
-      do m = 1, jmax_in
-        expmul(:)      = expmul(:) * expphi(:)
-        data_out(:,it) = data_out(:,it) + 2 * c2r_fn( expmul(:) * sumLege(m) )
-      end do
+    do ip = 1, 2*nth
+      expphi(ip) = exp( cunit * (ip-1) * pi / nth )
     end do
+    
+    do it = 1, nth
+      costheta = cos(pi/nth * (it-0.5_dbl)) ; expmul = cone
       
+      sumLege(:) = czero
+        do im = 0, jmax_in
+          call dpmm_sub( costheta, jmax_in, im, polyLege )
+          
+          do ij = im, jmax_in
+            sumLege(im) = sumLege(im) + spectra_in(ij*(ij+1)/2+im+1) * polyLege(ij-im+1)
+          end do
+        end do
+      
+      data_out(:,it) = c2r_fn( sumLege(0) )
+        do concurrent ( im = 1:jmax_in , ip = 1:2*nth )
+          expmul(ip)      = expmul(ip) * expphi(ip)
+          data_out(ip,it) = data_out(ip,it) + 2 * c2r_fn( expmul(ip) * sumLege(im) )
+        end do
+    end do
+    
     deallocate( sumLege, expphi, expmul, polyLege )
-      
+    
   end subroutine harmsy_sub
   
   subroutine harmsy_Pj0_sub(jmax_in, spectra_in, data_out)
