f85acf60850f9985a913291f4940f746a60c65cb
kvorka <jkdarkorbit@gmail.com>
2023-12-26 11:06:28 +0100

    fancy matrices object files handling

--- kernels/equations/matrices/Matrices.f90
+++ kernels/equations/matrices/Matrices.f90
@@ -15,107 +15,28 @@ module Matrices
     
   end type T_matrices
   
-  contains
-  
-  pure subroutine init_matrices_sub(this, nd, jmax, grid_type)
-    class(T_matrices), intent(inout) :: this
-    integer,           intent(in)    :: nd, jmax
-    character(len=*),  intent(in)    :: grid_type
+  interface
+    module pure subroutine init_matrices_sub(this, nd, jmax, grid_type)
+      class(T_matrices), intent(inout) :: this
+      integer,           intent(in)    :: nd, jmax
+      character(len=*),  intent(in)    :: grid_type
+    end subroutine init_matrices_sub
     
-    this%nd        = nd
-    this%jmax      = jmax
-    this%grid_type = grid_type
+    module pure subroutine deallocate_matrices_sub(this)
+      class(T_matrices), intent(inout) :: this
+    end subroutine deallocate_matrices_sub
+
+    module pure subroutine init_mtemp_sub(this)
+      class(T_matrices), intent(inout) :: this
+    end subroutine init_mtemp_sub
     
-  end subroutine init_matrices_sub
-  
-  pure subroutine init_mtemp_sub(this)
-    class(T_matrices), intent(inout) :: this
-    integer                          :: j
+    module pure subroutine init_mtorr_sub(this)
+      class(T_matrices), intent(inout) :: this
+    end subroutine init_mtorr_sub
     
-    allocate( this%temp(0:this%jmax) )
-    
-    select case (this%grid_type)
-      case('chebv')
-        do j = 0, this%jmax
-          call this%temp(j)%init_sub(3*this%nd+1, 5, 5)
-        end do
-      
-      case('homog')
-        do j = 0, this%jmax
-          call this%temp(j)%init_sub(3*this%nd+1, 3, 3)
-        end do
-    end select
-    
-  end subroutine init_mtemp_sub
-  
-  pure subroutine init_mtorr_sub(this)
-    class(T_matrices), intent(inout) :: this
-    integer                          :: j
-    
-    allocate( this%torr(1:this%jmax) )
-    
-    select case (this%grid_type)
-      case('chebv')
-        do j = 1, this%jmax
-          call this%torr(j)%init_sub(3*this%nd+1, 5, 5)
-        end do
-        
-      case('homog')
-        do j = 1, this%jmax
-          call this%torr(j)%init_sub(3*this%nd+1, 3, 3)
-        end do
-    end select
-    
-  end subroutine init_mtorr_sub
-  
-  pure subroutine init_mmech_sub(this)
-    class(T_matrices), intent(inout) :: this
-    integer                          :: j
-    
-    allocate( this%mech(1:this%jmax) )
-    
-    select case (this%grid_type)
-      case('chebv')
-        do j = 1, this%jmax
-          call this%mech(j)%init_sub(6*this%nd+2, 11, 11)
-        end do
-      
-      case('homog')
-        do j = 1, this%jmax
-          call this%mech(j)%init_sub(6*this%nd+2, 7, 7)
-        end do
-    end select
-      
-  end subroutine init_mmech_sub
-  
-  pure subroutine deallocate_matrices_sub(this)
-    class(T_matrices), intent(inout) :: this
-    integer                          :: j
-    
-    if ( allocated(this%temp) ) then
-      do j = 0, this%jmax
-        call this%temp(j)%deallocate_sub()
-      end do
-      
-      deallocate( this%temp )
-    end if
-    
-    if ( allocated(this%torr) ) then
-      do j = 1, this%jmax
-        call this%torr(j)%deallocate_sub()
-      end do
-      
-      deallocate( this%torr )
-    end if
-    
-    if ( allocated(this%mech) ) then
-      do j = 1, this%jmax
-        call this%mech(j)%deallocate_sub()
-      end do
-      
-      deallocate( this%mech )
-    end if
-    
-  end subroutine deallocate_matrices_sub
+    module pure subroutine init_mmech_sub(this)
+      class(T_matrices), intent(inout) :: this
+    end subroutine init_mmech_sub
+  end interface
   
 end module Matrices
