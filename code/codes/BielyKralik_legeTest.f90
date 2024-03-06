97820db9b1099fa0bc889fd9d9c0ed0263ce1e16
kvorka <jkdarkorbit@gmail.com>
2024-03-06 18:37:25 +0100

    this seems to be working

--- code/codes/BielyKralik_legeTest.f90
+++ code/codes/BielyKralik_legeTest.f90
@@ -2,10 +2,39 @@ program BielyKralik_legeTest
   use SphericalHarmonics
   implicit none
 
-  type(T_lateralGrid) :: sph
+  type(T_lateralGrid)            :: sph
+  integer                        :: jcut, j, m, jm
+  real(kind=dbl)                 :: xrand
+  complex(kind=dbl), allocatable :: scal1(:), scal2(:), scal3(:)
   
   !Inicializuj vypocet
-  call sph%init_sub(105)
+  jcut = 105
+  
+  call sph%init_sub(jcut)
+  
+  allocate( scal1(jcut*(jcut+1)/2+jcut+1), scal2(jcut*(jcut+1)/2+jcut+1), scal3(jcut*(jcut+1)/2+jcut+1) )
+  
+  scal1 = czero
+  scal2 = czero ; scal2(1) = cmplx(sqrt(4*pi), 0._dbl, kind=dbl)
+  scal3 = czero
+  
+  do j = 0, jcut
+    do m = 0, j
+      jm = j*(j+1)/2+m+1
+      
+      if ( m /= 0 ) then
+        call random_number( xrand ) ; scal1(jm)%re = xrand
+        call random_number( xrand ) ; scal1(jm)%im = xrand
+      else
+        call random_number( xrand ) ; scal1(jm)%re = xrand
+        scal1(jm)%im = zero
+      end if
+    end do
+  end do
+
+  call sph%vcsum_sub( scal1, scal2, scal3 )
+
+  write(*,*) 'vcsum: ', maxval(abs(scal1(:)-scal3(:)))
   
   !Cistenie
   call sph%deallocate_sub()
