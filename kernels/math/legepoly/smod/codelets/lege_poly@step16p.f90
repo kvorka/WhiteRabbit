a0dc4926eaface8bfb0c2eb89950585986b3bfee
kvorka <jkdarkorbit@gmail.com>
2024-10-23 17:49:24 +0200

    updated recurrence, needs benchmark

--- kernels/math/legepoly/smod/codelets/lege_poly@step16p.f90
+++ kernels/math/legepoly/smod/codelets/lege_poly@step16p.f90
@@ -35,9 +35,9 @@ submodule (lege_poly) step16p
     integer                       :: i2
     
     do concurrent ( i2=1:16 )
-      pmj2(i2) = pmj1(i2)
-      pmj1(i2) = pmj0(i2)
-      pmj0(i2) = this%amj(mj) * cosx(i2) * pmj1(i2) - this%bmj(mj) * pmj2(i2)
+      pmj2(i2) = this%bmj(mj) * pmj1(i2)
+      pmj1(i2) = this%amj(mj) * pmj0(i2)
+      pmj0(i2) = cosx(i2) * pmj1(i2) - pmj2(i2)
     end do
     
   end subroutine recursion_16_sub
--- kernels/math/legepoly/smod/init/lege_poly@coeffs.f90
+++ kernels/math/legepoly/smod/init/lege_poly@coeffs.f90
@@ -16,11 +16,17 @@ submodule (lege_poly) coeffs
         this%cmm(im) = -sqrt( (2*im+one) / (2*im) )
       end if
       
+      ij = im
+        imj = im*(this%jmax+1)-im*(im+1)/2+ij+1
+        
+        this%amj(imj) = one
+        this%bmj(imj) = one
+      
       do ij = im+1, this%jmax
         imj = im*(this%jmax+1)-im*(im+1)/2+ij+1
         
         this%amj(imj) = sqrt((2*ij-1)*(2*ij+one)                    /(         (ij-im)*(ij+im)))
-        this%bmj(imj) = sqrt(         (2*ij+one)*(ij-im-1)*(ij+im-1)/((2*ij-3)*(ij-im)*(ij+im)))
+        this%bmj(imj) = sqrt(         (2*ij+one)*(ij-im-1)*(ij+im-1)/((2*ij-3)*(ij-im)*(ij+im))) / this%amj(imj-1)
       end do
     end do
     
