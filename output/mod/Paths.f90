a60ccbe679cc07a79556d49da0dc020ca95774e1
kvorka <jkdarkorbit@gmail.com>
2025-07-20 19:34:47 +0200

    adding output for temperature and alignement set up

--- output/mod/Paths.f90
+++ output/mod/Paths.f90
@@ -9,7 +9,7 @@ module Paths
   character(len=*), parameter :: path_nuss         = 'data/Nuss.dat'
   character(len=*), parameter :: path_ocean_temp   = 'data/data_ocean_temp/Temp-'
   character(len=*), parameter :: path_ocean_velc   = 'data/data_ocean_veloc/Velc-'
-  character(len=*), parameter :: path_ocean_flux   = 'data/data_ocean_flux/Flux-'
+  character(len=*), parameter :: path_ocean_flux   = 'data/data_ocean_fluxu/Fluxu-'
   character(len=*), parameter :: path_ice_shape_dn = 'data/data_ice_shape/Shape_dn-'
   character(len=*), parameter :: path_ice_topo_dn  = 'data/data_ice_topo/Topo_dn-'
   character(len=*), parameter :: path_ice_shape_up = 'data/data_ice_shape/Shape_up-'
